77a8364dc224572d270a6546f363a12730ee5e81
jrogado17 <j.rogado17@yahoo.com>
2023-04-10 09:44:37 -1000

    user input working but not showing up in html

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
+
+// container of all inputls
 const userInputs = document.querySelector("#user-inputs");
-const houseType = document.querySelector("#type");
-const housePrice = document.querySelector("#house");
-const houseState = document.querySelector("#state");
-const houseAddress = document.querySelector("#address");
-const numBedrooms = document.querySelector("#bedrooms");
-const numBathrooms = document.querySelector("#bathrooms");
-const squareFootage = document.querySelector("#square-footage");
-const listingButton = document.querySelector("#submit");
+
+// submit button
+const listingButton = document.querySelector("form");
+const listingsList = document.createElement("ul");
+
+//appending
+listingDisplay.appendChild(listingsList);
+
 
 const houseListing = [];
+listingButton.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //user inputs
 
-const buildAHouse = () => {
-  listingsList.innerHTML = "";
-  houseListing.forEach((listing) => {
-    const li = document.createElement("li");
-    li.appendChild(createListing(listing));
-    listingsList.appendChild(li);
-    console.log("house:", houseListing);
-  });
-};
-
-listingButton.addEventListener("click", function () {});
-
-function createListing(newHouse) {
-  const listingsList = document.createElement("ul");
-  for (const key in newHouse) {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${key}: ${[newHouse]}`;
-    listingsList.appendChild(listItem);
+  const houseType = document.querySelector("#type").value;
+  const housePrice = document.querySelector("#price").value;
+  const houseState = document.querySelector("#state").value;
+  const houseAddress = document.querySelector("#address").value;
+  const numBedrooms = document.querySelector("#bedrooms").value;
+  const numBathrooms = document.querySelector("#bathrooms").value;
+  const squareFootage = document.querySelector("#square-footage").value;
+  //reset input values
+  document.querySelector("#type").value = "";
+  document.querySelector("#price").value = "";
+  document.querySelector("#state").value = "";
+  document.querySelector("#address").value = "";
+  document.querySelector("#bedrooms").value = "";
+  document.querySelector("#bathrooms").value = "";
+  document.querySelector("#square-footage").value = "";
+  
+  const listing = {
+    houseType: houseType,
+    housePrice: housePrice,
+    houseState: houseState,
+    houseAddress: houseAddress,
+    numBedrooms: numBedrooms,
+    numBathrooms: numBathrooms,
+    squareFootage: squareFootage,
   }
+  const houseContainer = document.createElement("div");
+  
+
+  listingsList.appendChild(houseContainer);
+  houseListing.push(listing);
+  renderListings()
+});
+
+
+function renderListings() {
+  listingDisplay.innerHTML = '';
+  const listingElement = document.createElement("li");
+  houseListing.forEach((listing) => {
+    listingElement.innerHTML = `
+      <h2>${listing.houseAddress}</h2>
+      <p>Property Type: ${listing.houseType}</p>
+      <p>City: ${listing.housePrice}</p>
+      <p>State: ${listing.houseState}</p>
+    `;
+    listingsList.appendChild(listingElement);
+  });
 }
+
--- listing.js
+++ listing.js
@@ -9,3 +9,4 @@ class Listing {
     this.squarefootage = props.squarefootage;
   }
 }
+
